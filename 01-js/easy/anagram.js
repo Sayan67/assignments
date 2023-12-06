17a5a0b11a8ea51cf6952c440486c0116c2c1796
Sayan67 <sayan.das.21@aot.edu.in>
2023-12-07 00:24:14 +0530

    Easy Done

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -1,11 +1,40 @@
-/*
-  Write a function `isAnagram` which takes 2 parameters and returns true/false if those are anagrams or not.
-  What's Anagram?
-  - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
-*/
+// /*
+//   Write a function `isAnagram` which takes 2 parameters and returns true/false if those are anagrams or not.
+//   What's Anagram?
+//   - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
+// */
+function getAllPermutations(str){
+  if(str.length <= 1){
 
-function isAnagram(str1, str2) {
-
+    return [str];
+  }
+  const permutations=[];
+  for(let i=0; i < str.length; i++){
+      const firstVal = str[i];
+      const remaining = str.slice(0, i) + str.slice(i + 1);
+      const subPerms = getAllPermutations(remaining);
+      
+      for(const perms of subPerms){
+        permutations.push(firstVal + perms);
+      }
+  }
+  return permutations;
 }
 
+function isAnagram(str1, str2) {
+    if(typeof str1 === "string" && typeof str2 === "string"){
+      const permArray = getAllPermutations(str1.toLowerCase());
+      //console.log(permArray);
+      for (let i of permArray) {
+        if (i===str2.toLowerCase()) {
+          return true;
+        }
+      }
+      return false;
+    }else{
+      return "Invalid input";
+    }
+}
+console.log(isAnagram('Bad Credit','Debit Card'));
 module.exports = isAnagram;
+
