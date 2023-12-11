02173e2f2f2e60228c1002abedb5bfad2d8ab3ef
Sayan67 <sayan.das.21@aot.edu.in>
2023-12-11 14:50:29 +0530

    Done medium 3

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -9,5 +9,15 @@ There is no automated test for this one, this is more for you to understand time
 */
 
 function calculateTime(n) {
-    return 0.01;
+    let res = 0;
+    for(let i=1;i<=n;i++){
+        res +=i;
+    }
+    const end = new Date().getTime();
+    console.log(start);
+    console.log(end);
+    return (end-start);
 }
+
+const start = new Date().getTime();
+console.log(calculateTime(100000000));
